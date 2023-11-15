b2f22860b107eb228c375976a3e74872a833d641
Wellington Suero <144365343+Wellfc@users.noreply.github.com>
2023-11-15 15:02:28 -0600

    Added js functionality

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const container = document.querySelector('.login__container');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
+
+registerLink.addEventListener('click', () => {
+    container.classList.add('active');
+});
+
+loginLink.addEventListener('click', () => {
+    container.classList.remove('active');
+});
+
+btnPopup.addEventListener('click', () => {
+    modal.classList.add('active-popup');
+    container.classList.add('active-popup');
+});
+
+iconClose.addEventListener('click', () => {
+    modal.classList.remove('active-popup');
+    container.classList.remove('active-popup');
+});
